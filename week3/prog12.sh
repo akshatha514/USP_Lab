09131fdd89a8514df442dada567f83c4754b41a6
akshatha514 <71319744+akshatha514@users.noreply.github.com>
2021-11-28 20:44:46 +0530

    Delete week3 directory

--- week3/prog12.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the value of n"
-read n
-sum=0
-for((i=1;i<=n;i++))
-do
-sum=$((sum+i))
-done
-echo "Sum of $n natural numbers is: $sum"
-
--- week3/prog9.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the value of n"
-read n
-i=2
-while [ $i -le $n ]
-do
-sum=$((sum+i))
-i=$((i+2))
-done
-echo "Sum of even numbers upto n is $sum"
